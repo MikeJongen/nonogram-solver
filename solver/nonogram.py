0b0f03c0eca5379b19e21149526703ffeae6723d
Mike Jongen <mikejongen@hotmail.com>
2020-02-29 22:52:07 +0100

    Fix for print_solution function, still used old variables. Updated print_clues to better readable.

--- solver/nonogram.py
+++ solver/nonogram.py
@@ -42,19 +42,19 @@ class Nonogram:
             self.clues[cur_axis][index] = clue
 
     def print_solution(self):
-        top_row = "+" + self.size_x * "--+"
+        top_row = "+" + self.size[self.x] * "--+"
         print(top_row)
-        for y in range(self.size_y):
+        for y in range(self.size[self.y]):
             row = "|"
-            for x in range(self.size_x):
+            for x in range(self.size[self.x]):
                 row += f"{self.printable_values[self.solution[x][y]]}|"
             print(row)
             print(top_row)
 
     def print_clues(self):
         print("Horizontal clues:")
-        for clue in self.clues[self.axis["x"]]:
+        for clue in self.clues[self.x]:
             print(clue)
         print("Vertical clues:")
-        for clue in self.clues[self.axis["y"]]:
+        for clue in self.clues[self.y]:
             print(clue)
