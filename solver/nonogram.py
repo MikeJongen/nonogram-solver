79295e52dc97fd9820e87c97a6b12f5ca3e85d6f
Mike Jongen <mikejongen@hotmail.com>
2020-02-29 21:58:11 +0100

    Changed variables which have a x and y variant to lists. Made a general function for setting clues.

--- solver/nonogram.py
+++ solver/nonogram.py
@@ -7,33 +7,39 @@ class Nonogram:
     printable_values = {-1: "  ",
                          0: "..",
                          1: "XX"}
+    axis = {"x": 0,
+            "y": 1}
+
+    x = axis["x"]
+    y = axis["y"]
 
     def __init__(self, size_x, size_y):
-        self.size_x = size_x
-        self.size_y = size_y
+        self.size = [0, 0]
+        self.size[self.x] = size_x
+        self.size[self.y] = size_y
 
-        self.solution = [[-1 for y in range(self.size_y)] \
-                            for x in range(self.size_x) ]
-        self.clues_x = [[] for x in range(self.size_x)]
-        self.clues_y = [[] for y in range(self.size_y)]
+        self.solution = [[-1 for y in range(self.size[self.y])]\
+                             for x in range(self.size[self.x])]
+        self.clues = [0, 0]
+        self.clues[self.x] = [[] for x in range(self.size[self.x])]
+        self.clues[self.y] = [[] for y in range(self.size[self.y])]
 
     def set_clues_x(self, *clues):
-        if len(clues) != self.size_x:
-            raise ValueError
-        for index, clue in enumerate(clues):
-            min_length_clue = sum(clue) + len(clue) - 1
-            if(min_length_clue > self.size_x):
-                raise ValueError
-            self.clues_x[index] = clue
+        return self.set_clues("x", *clues)
 
     def set_clues_y(self, *clues):
-        if len(clues) != self.size_y:
+        return self.set_clues("y", *clues)
+
+    def set_clues(self, input_axis, *clues):
+        cur_axis = self.axis[input_axis]
+
+        if len(clues) != self.size[cur_axis]:
             raise ValueError
         for index, clue in enumerate(clues):
             min_length_clue = sum(clue) + len(clue) - 1
-            if(min_length_clue > self.size_y):
+            if(min_length_clue > self.size[cur_axis]):
                 raise ValueError
-            self.clues_y[index] = clue
+            self.clues[cur_axis][index] = clue
 
     def print_solution(self):
         top_row = "+" + self.size_x * "--+"
@@ -47,8 +53,8 @@ class Nonogram:
 
     def print_clues(self):
         print("Horizontal clues:")
-        for clue in self.clues_x:
+        for clue in self.clues[self.axis["x"]]:
             print(clue)
         print("Vertical clues:")
-        for clue in self.clues_y:
+        for clue in self.clues[self.axis["y"]]:
             print(clue)
