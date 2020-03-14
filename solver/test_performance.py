2fd571fc357c0527b4e5591b5d3bf5332c8e4185
Mike Jongen <mikejongen@hotmail.com>
2020-03-14 20:50:30 +0100

    Added 3 easy puzzles. Added script that tests a solver and shows the performance of the solver.

---
+++ solver/test_performance.py
@@ -0,0 +1,31 @@
+import sys
+import os
+test_path = os.path.join(os.path.dirname(__file__), '../')
+sys.path.insert(0, os.path.abspath(test_path))
+
+import timeit
+import solver.basic
+
+def run():
+    puzzle.solver1()
+
+def run_single_puzzle(puzzle):
+    iterations = 10000
+    time = timeit.timeit("run()", \
+                         setup="from __main__ import run", \
+                         number=iterations)
+    complete = puzzle.is_complete()
+    print("Solved: {}".format(bool(complete)))
+    if complete == 0:
+        print("Percentage: {:5.2f}%".format(puzzle.percent_complete()))
+    print("Time = {:8.5f} ms".format(time / iterations * 1000))
+
+if __name__ == '__main__':
+    # make puzzle global so timeit can access it
+    global puzzle
+
+    for filename in os.listdir("puzzles/easy"):
+        puzzle_file = "puzzles/easy/" + filename
+        puzzle = solver.basic.BasicSolver(file=puzzle_file)
+        print("\nPuzzle: " + filename.strip(".txt"))
+        run_single_puzzle(puzzle)
