ded62268004465dece729f344913bdbbf7dfcc7a
Mike Jongen <mikejongen@hotmail.com>
2021-02-28 02:11:29 +0100

    solver1 uses BasicRowSolver class.

--- solver/basic.py
+++ solver/basic.py
@@ -13,35 +13,20 @@ class BasicSolver(Nonogram):
         """only solves rows/columns which have one possible solution"""
         for index in range(self.size[self.y]):
             try:
-                self.solve_defined_row("x", index)
+                row = BasicRowSolver(
+                    *self.get_clue_solution_pair("x", index))
+                row_solution = row.solve_defined_row()
+                self._set_solution_row(self.x, index, row_solution)
             except SolveError:
                 pass
         for index in range(self.size[self.x]):
             try:
-                self.solve_defined_row("y", index)
+                row = BasicRowSolver(
+                    *self.get_clue_solution_pair("y", index))
+                row_solution = row.solve_defined_row()
+                self._set_solution_row(self.y, index, row_solution)
             except SolveError:
                 pass
-
-    def solve_defined_row(self, input_axis, index):
-        """Solves a row/column that has only one possible solution"""
-        cur_axis = self.axis[input_axis]
-        cur_clue = self.clues[cur_axis][index]
-        length_clue = sum(cur_clue) + len(cur_clue) - 1
-
-        if cur_clue == []:
-            # empty row
-            row_solution = [-1 for i in range(self.size[cur_axis])]
-        elif length_clue == self.size[cur_axis]:
-            # clue fills entire row
-            row_solution = []
-            for block in cur_clue:
-                row_solution.extend([1 for i in range(block)])
-                row_solution.append(-1)
-            del row_solution[-1]
-        else:
-            raise SolveError
-
-        self._set_solution_row(cur_axis, index, row_solution)
 
     def fill_common_elements_row(self, input_axis, index):
         """Fills all elements which are common for all solutions"""
@@ -149,3 +134,5 @@ class BasicRowSolver(Row):
             del self.values[-1]
         else:
             raise SolveError
+
+        return self.values
--- test/test_basic.py
+++ test/test_basic.py
@@ -19,49 +19,6 @@ class TestBasic(unittest.TestCase):
                              [0,  1,  0,  0,  1]]
         self.assertEqual(new_nonogram.solution, expected_solution)
 
-    def test_defined_row_y(self):
-        new_nonogram = basic.BasicSolver(5, 10)
-        clues_x = [[1, 3], [1, 1, 1], [1, 3], [1, 1, 1], [1, 2],
-                   [1, 1, 1], [1, 3], [1, 1], [1, 2], [1, 3]]
-        clues_y = [[10], [], [4, 2, 2], [1, 1, 1, 1, 2], [8, 1]]
-        new_nonogram.set_clues_x(*clues_x)
-        new_nonogram.set_clues_y(*clues_y)
-        new_nonogram.solve_defined_row('y', 0)
-        new_nonogram.solve_defined_row('y', 1)
-        new_nonogram.solve_defined_row('y', 2)
-        new_nonogram.solve_defined_row('y', 3)
-        new_nonogram.solve_defined_row('y', 4)
-        expected_solution = [[1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
-                             [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
-                             [1,  1,  1,  1, -1,  1,  1, -1,  1,  1],
-                             [1, -1,  1, -1,  1, -1,  1, -1,  1,  1],
-                             [1,  1,  1,  1,  1,  1,  1,  1, -1,  1]]
-        self.assertEqual(new_nonogram.solution, expected_solution)
-
-    def test_defined_row_x(self):
-        new_nonogram = basic.BasicSolver(10, 5)
-        clues_y = [[1, 3], [1, 1, 1], [1, 3], [1, 1, 1], [1, 2],
-                   [1, 1, 1], [1, 3], [1, 1], [1, 2], [1, 3]]
-        clues_x = [[10], [], [4, 2, 2], [1, 1, 1, 1, 2], [8, 1]]
-        new_nonogram.set_clues_x(*clues_x)
-        new_nonogram.set_clues_y(*clues_y)
-        new_nonogram.solve_defined_row('x', 0)
-        new_nonogram.solve_defined_row('x', 1)
-        new_nonogram.solve_defined_row('x', 2)
-        new_nonogram.solve_defined_row('x', 3)
-        new_nonogram.solve_defined_row('x', 4)
-        expected_solution = [[1, -1,  1,  1,  1],
-                             [1, -1,  1, -1,  1],
-                             [1, -1,  1,  1,  1],
-                             [1, -1,  1, -1,  1],
-                             [1, -1, -1,  1,  1],
-                             [1, -1,  1, -1,  1],
-                             [1, -1,  1,  1,  1],
-                             [1, -1, -1, -1,  1],
-                             [1, -1,  1,  1, -1],
-                             [1, -1,  1,  1,  1]]
-        self.assertEqual(new_nonogram.solution, expected_solution)
-
     def test_fillcommonelementsrow(self):
         new_nonogram = basic.BasicSolver(10, 5)
         clues_y = [[4], [2, 2], [5], [5], [2, 1],
