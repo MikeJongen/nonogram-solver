9ab715379a85b3630922083e8ef353acdf1cbe49
Mike Jongen <mikejongen@hotmail.com>
2021-02-28 11:56:38 +0100

    Use classes in main.

--- solver/test_performance.py
+++ solver/test_performance.py
@@ -60,32 +60,13 @@ class Test_Runner:
                 self.correct_results[puzzle]["done_pct"]))
 
 
-def run():
-    puzzle.reset_solution()
-    puzzle.solver1()
-
-
-def run_single_puzzle(puzzle):
-    iterations = 10000
-    time = timeit.timeit("run()",
-                         setup="from __main__ import run",
-                         number=iterations)
-    complete = puzzle.is_complete()
-    print("Solved: {}".format(bool(complete)))
-    if complete == 0:
-        print("Percentage: {:5.2f}%".format(puzzle.percent_complete()))
-    else:
-        correct = puzzle.is_correct()
-        print("Correct: {}".format(correct))
-    print("Time = {:8.5f} ms".format(time / iterations * 1000))
-
-
 if __name__ == '__main__':
-    # make puzzle global so timeit can access it
-    global puzzle
+    testrunner = Test_Runner(solver.basic.BasicSolver)
 
     for filename in os.listdir("puzzles/easy"):
         puzzle_file = "puzzles/easy/" + filename
-        puzzle = solver.basic.BasicSolver(file=puzzle_file)
+        testrunner.run_correctness_test(puzzle_file)
+        testrunner.run_timing_test(puzzle_file)
         print("\nPuzzle: " + filename.strip(".txt"))
-        run_single_puzzle(puzzle)
+        testrunner.print_correctness(puzzle_file)
+        testrunner.print_timing(puzzle_file)
