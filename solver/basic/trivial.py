4fbf0ceeecfb7fbf25da908e2cd47939051aa56a
Mike Jongen <mikejongen@hotmail.com>
2021-03-02 15:52:30 +0100

    Use new row variables. removed raise error if failed, should be ok to fail.

--- solver/basic/trivial.py
+++ solver/basic/trivial.py
@@ -14,21 +14,17 @@ class TrivialSolver(Nonogram):
     def solve(self):
         """only solves rows/columns which have one possible solution"""
         for index in range(self.size["y"]):
-            try:
-                row = TrivialRowSolver(
-                    *self.get_clue_solution_pair("x", index))
-                row_solution = row.solve_defined_row()
-                self._set_solution_row("x", index, row_solution)
-            except SolveError:
-                pass
+            row = TrivialRowSolver(
+                *self.get_clue_solution_pair("x", index))
+            changed = row.solve_defined_row()
+            if changed:
+                self._set_solution_row("x", index, row.values)
         for index in range(self.size["x"]):
-            try:
-                row = TrivialRowSolver(
-                    *self.get_clue_solution_pair("y", index))
-                row_solution = row.solve_defined_row()
-                self._set_solution_row("y", index, row_solution)
-            except SolveError:
-                pass
+            row = TrivialRowSolver(
+                *self.get_clue_solution_pair("y", index))
+            changed = row.solve_defined_row()
+            if changed:
+                self._set_solution_row("y", index, row.values)
 
 
 class TrivialRowSolver(Row):
@@ -37,23 +33,24 @@ class TrivialRowSolver(Row):
         RowSolver for TrivialSolver class
 
         Solves a row/column that has only one possible solution
-        """
-        if self.is_complete():
-            return self.values
 
-        length_clue = sum(self.clues) + len(self.clues) - 1
+        returnvalue : Bool
+            A bool to indicate if the row has been changed. (true if changed)
+        """
+        if self.solved:
+            return self.values
 
         if self.clues == []:
             # empty row
             self.values = [-1 for i in range(self.size)]
-        elif length_clue == self.size:
+            self.solved = True
+        elif self.clue_size == self.size:
             # clue fills entire row
             self.values = []
             for block in self.clues:
                 self.values.extend([1 for i in range(block)])
                 self.values.append(-1)
             del self.values[-1]
-        else:
-            raise SolveError
+            self.solved = True
 
-        return self.values
+        return self.solved
