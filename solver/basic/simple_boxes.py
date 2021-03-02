cacb6c49797d18c41b17a161c7ceeb122d853b0b
Mike Jongen <mikejongen@hotmail.com>
2021-03-02 17:09:19 +0100

    Added solver for simple boxes algorithm.

---
+++ solver/basic/simple_boxes.py
@@ -0,0 +1,73 @@
+from solver.nonogram import Nonogram
+from solver.nonogram import Row
+from solver.error import *
+
+
+class SimpleBoxesSolver(Nonogram):
+    """
+    Solver class to apply simple boxes algorithm.
+    """
+
+    def solve(self):
+        """only solves rows/columns which have one possible solution"""
+        for index in range(self.size["y"]):
+            row = SimpleBoxesRowSolver(
+                *self.get_clue_solution_pair("x", index))
+            changed = row.solve_defined_row()
+            if changed:
+                self._set_solution_row("x", index, row.values)
+        for index in range(self.size["x"]):
+            row = SimpleBoxesRowSolver(
+                *self.get_clue_solution_pair("y", index))
+            changed = row.solve_defined_row()
+            if changed:
+                self._set_solution_row("y", index, row.values)
+
+
+class SimpleBoxesRowSolver(Row):
+    def solve_simple_boxes(self):
+        """
+        RowSolver for SimpleBoxesSolver class
+
+        Applies simple boxes algorithm to row
+
+        returnvalue : Bool
+            A bool to indicate if the row has been changed. (true if changed)
+        """
+        if self.solved:
+            return self.values
+
+        movement_space = self.size - self.clue_size
+
+        if movement_space >= max(self.clues):
+            # clues too small for simple boxes to work
+            return False
+
+        for index, clue in enumerate(self.clues):
+            if movement_space < clue:
+                self._simple_boxes_clue(index, clue)
+
+        return self.solved
+
+    def _simple_boxes_clue(self, index, clue):
+        """
+        Apply simple box for clue at given index
+        """
+
+        # lowest row index where clue can be placed
+        minimum = 0
+        for other_clue in self.clues[:index]:
+            minimum += other_clue
+            minimum += 1  # space between clues
+
+        # highest row index where clue can be placed
+        maximum = self.size
+        for other_clue in self.clues[index+1:]:
+            maximum -= other_clue
+            maximum -= 1  # space between clues
+
+        # start and stop index of the spaces that need to be filled
+        start_fill = maximum - clue
+        stop_fill = minimum + clue
+
+        self.values[start_fill:stop_fill] = [1] * (stop_fill - start_fill)
---
+++ test/test_simpleboxes.py
@@ -0,0 +1,78 @@
+import sys
+import os
+test_path = os.path.join(os.path.dirname(__file__), '../')  # nopep8
+sys.path.insert(0, os.path.abspath(test_path))  # nopep8
+
+from solver.error import *
+import solver.basic.simple_boxes as simple_boxes
+import unittest
+
+
+class TestSimpleBoxes(unittest.TestCase):
+    def test_solve_clues_too_small(self):
+        empty = [0] * 15
+        row = simple_boxes.SimpleBoxesRowSolver(
+            [5, 4], empty)
+        row.solve_simple_boxes()
+        self.assertEqual(
+            row.values, empty)
+
+    def test_solve_clues_just_big_enough(self):
+        empty = [0] * 15
+        expected = [0] * 15
+        expected[7] = 1
+        row = simple_boxes.SimpleBoxesRowSolver(
+            [2, 5, 2], empty)
+        row.solve_simple_boxes()
+        self.assertEqual(
+            row.values, expected)
+
+    def test_solve_clues_just_big_enough_asymetric(self):
+        empty = [0] * 15
+        expected = [0] * 15
+        expected[6] = 1
+        row = simple_boxes.SimpleBoxesRowSolver(
+            [1, 5, 3], empty)
+        row.solve_simple_boxes()
+        self.assertEqual(
+            row.values, expected)
+
+    def test_solve_clues_lot_of_numbers(self):
+        empty = [0] * 20
+        expected = [0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0]
+        row = simple_boxes.SimpleBoxesRowSolver(
+            [1, 2, 1, 5, 3, 2], empty)
+        row.solve_simple_boxes()
+        self.assertEqual(
+            row.values, expected)
+
+    def test_solve_clues_single_just_big_enough(self):
+        empty = [0] * 15
+        expected = [0] * 15
+        expected[7] = 1
+        row = simple_boxes.SimpleBoxesRowSolver(
+            [8], empty)
+        row.solve_simple_boxes()
+        self.assertEqual(
+            row.values, expected)
+
+    def test_solve_clues_single(self):
+        empty = [0] * 15
+        expected = [0] * 15
+        expected[4:11] = [1] * 7
+        row = simple_boxes.SimpleBoxesRowSolver(
+            [11], empty)
+        row.solve_simple_boxes()
+        self.assertEqual(
+            row.values, empty)
+
+    def test_solve_defined_row(self):
+        empty = [0] * 10
+        row = simple_boxes.SimpleBoxesRowSolver(
+            [5, 4], empty)
+        row.solve_simple_boxes()
+        self.assertEqual(row.values, [1, 1, 1, 1, 1, 0, 1, 1, 1, 1])
+
+
+if __name__ == '__main__':
+    unittest.main()
