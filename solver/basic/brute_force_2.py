7801a7460b639412da2a1f673679f584d6a3e65d
Mike Jongen <mikejongen@hotmail.com>
2021-03-16 14:12:05 +0100

    Added improved brute force solver.

---
+++ solver/basic/brute_force_2.py
@@ -0,0 +1,57 @@
+from solver.nonogram import Nonogram, Row
+from solver.basic.brute_force import BruteForceRowSolver
+
+
+class BruteForceSolver2(Nonogram):
+    """Brute force solver class
+
+    Performance heavy algorithm, use as last option.
+    Improved version of brute force. Uses more memory though.
+    """
+
+    def init_row_solvers(self):
+        Nonogram.init_row_solvers(self, BruteForceRowSolver2)
+
+    def solve(self):
+        """
+        Returns True if anything was changed to the solution.
+        """
+        return self.solve_single_iteration(BruteForceRowSolver2.solve_brute_force_save_intermediate)
+
+
+class BruteForceRowSolver2(BruteForceRowSolver, Row):
+    def solve_brute_force_save_intermediate(self, maximum_solutions=100000):
+        """
+        RowSolver for BruteForceSaveIntermediateSolver class
+
+        Tests every possible solution.
+
+        maximum_solutions : int
+            only solve if total number of solutions is lower than this.
+        returnvalue : Bool
+            A bool to indicate if the row has been changed. (True if changed)
+        """
+        if self.solved:
+            return False
+
+        if not hasattr(self, 'number_of_solutions'):
+            self.number_of_solutions = self._get_number_of_solutions()
+        if self.number_of_solutions > maximum_solutions:
+            return False
+
+        if not hasattr(self, 'all_solutions'):
+            self.all_solutions = self._get_all_solutions()
+        new_solution = None
+        for solution in self.all_solutions:
+            if self._check_solution(solution):
+                if new_solution is None:
+                    new_solution = solution
+                    continue
+                new_solution = self._get_matching_solution(
+                    new_solution, solution)
+        if self.values == new_solution:
+            return False
+        if new_solution is not None:
+            self.values = new_solution
+
+        return True
---
+++ test/test_brute_force_2.py
@@ -0,0 +1,123 @@
+import sys
+import os
+import unittest
+test_path = os.path.join(os.path.dirname(__file__), '../')   # noqa
+sys.path.insert(0, os.path.abspath(test_path))   # noqa
+
+import solver.basic.brute_force_2 as brute_force  # noqa: E402
+
+
+class TestBasic(unittest.TestCase):
+    def test_number_of_solutions(self):
+        row = brute_force.BruteForceRowSolver2([2, 1], [0] * 5)
+        no_solutions = row._get_number_of_solutions()
+        self.assertEqual(no_solutions, 3)
+
+    def test_number_of_solutions_large(self):
+        row = brute_force.BruteForceRowSolver2([1, 1, 1], [0] * 10)
+        no_solutions = row._get_number_of_solutions()
+        self.assertEqual(no_solutions, 56)
+
+    def test_number_of_solutions_large_2(self):
+        row = brute_force.BruteForceRowSolver2([2, 1, 1], [0] * 10)
+        no_solutions = row._get_number_of_solutions()
+        self.assertEqual(no_solutions, 35)
+
+    def test_number_of_solutions_one_clue(self):
+        row = brute_force.BruteForceRowSolver2([2], [0] * 5)
+        no_solutions = row._get_number_of_solutions()
+        self.assertEqual(no_solutions, 4)
+
+    def test_number_of_solutions_empty(self):
+        row = brute_force.BruteForceRowSolver2([], [0] * 5)
+        no_solutions = row._get_number_of_solutions()
+        self.assertEqual(no_solutions, 1)
+
+    def test_get_all_solutions(self):
+        row = brute_force.BruteForceRowSolver2([2, 1], [0] * 5)
+        expected_solutions = [
+            [1, 1, -1, 1, -1],
+            [1, 1, -1, -1, 1],
+            [-1, 1, 1, -1, 1],
+        ]
+        solutions = row._get_all_solutions()
+        self.assertEqual(solutions, expected_solutions)
+
+    def test_get_all_solutions_one_clue(self):
+        row = brute_force.BruteForceRowSolver2([3], [0] * 5)
+        expected_solutions = [
+            [1, 1, 1, -1, -1],
+            [-1, 1, 1, 1, -1],
+            [-1, -1, 1, 1, 1],
+        ]
+        solutions = row._get_all_solutions()
+        self.assertEqual(solutions, expected_solutions)
+
+    def test_get_all_solutions_two_clues(self):
+        row = brute_force.BruteForceRowSolver2([3, 4], [0] * 10)
+        solution = row._get_all_solutions()
+        expected_solution = [[1,  1, 1, -1,  1,  1, 1, 1, -1, -1],
+                             [1,  1, 1, -1, -1,  1, 1, 1,  1, -1],
+                             [1,  1, 1, -1, -1, -1, 1, 1,  1,  1],
+                             [-1,  1, 1,  1, -1,  1, 1, 1,  1, -1],
+                             [-1,  1, 1,  1, -1, -1, 1, 1,  1,  1],
+                             [-1, -1, 1,  1,  1, -1, 1, 1,  1,  1]]
+        self.assertEqual(expected_solution, solution)
+
+    def test_get_all_solutions_three_clues(self):
+        row = brute_force.BruteForceRowSolver2([2, 2, 3], [0] * 10)
+        solution = row._get_all_solutions()
+        expected_solution = [[1, 1, -1,  1, 1, -1,  1, 1, 1, -1],
+                             [1, 1, -1,  1, 1, -1, -1, 1, 1,  1],
+                             [1, 1, -1, -1, 1,  1, -1, 1, 1,  1],
+                             [-1, 1,  1, -1, 1,  1, -1, 1, 1,  1]]
+        self.assertEqual(expected_solution, solution)
+
+    def test_get_all_solutions_empty(self):
+        row = brute_force.BruteForceRowSolver2([], [0] * 5)
+        expected_solutions = [
+            [-1, -1, -1, -1, -1],
+        ]
+        solutions = row._get_all_solutions()
+        self.assertEqual(solutions, expected_solutions)
+
+    def test_check_solutions(self):
+        row = brute_force.BruteForceRowSolver2([2, 1], [1, 0, 0, 0, 0])
+        valid = row._check_solution([1, 1, 0, 1, 0])
+        self.assertTrue(valid)
+        valid = row._check_solution([0, 1, 1, 0, 1])
+        self.assertFalse(valid)
+
+    def test_matching_solutions(self):
+        row = brute_force.BruteForceRowSolver2([2, 1], [0] * 5)
+        solution1 = [-1, 1, 1, -1, 1]
+        solution2 = [1, 1, -1, -1, 1]
+        expected_solution = [0, 1, 0, -1, 1]
+        solution = row._get_matching_solution(solution1, solution2)
+        self.assertEqual(expected_solution, solution)
+
+    def test_row_brute_force_empty_values(self):
+        row = brute_force.BruteForceRowSolver2([2, 1], [0] * 5)
+        changed = row.solve_brute_force_save_intermediate()
+        self.assertTrue(changed)
+        self.assertEqual(row.values, [0, 1, 0, 0, 0])
+
+    def test_row_brute_force(self):
+        row = brute_force.BruteForceRowSolver2([2, 1], [1, 0, 0, 0, 0])
+        changed = row.solve_brute_force_save_intermediate()
+        self.assertTrue(changed)
+        self.assertEqual(row.values, [1, 1, -1, 0, 0])
+
+    def test_brute_force(self):
+        puzzle = brute_force.BruteForceSolver2(file="test/puzzles/stairs.json")
+        puzzle.solve()
+        puzzle.update_row_solvers()
+        puzzle.solve()
+        puzzle.update_row_solvers()
+        puzzle.solve()
+        self.assertTrue(puzzle.is_complete())
+        self.assertTrue(puzzle.is_correct())
+
+
+if __name__ == '__main__':
+    unittest.main()
